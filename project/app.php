cc666a6b0f5a317e4354ef890940536d4921d598
Piddubnyj <i.piddubnyj@betinvest.com>
2019-04-09 14:46:08 +0300

    master-sqlite

--- project/app.php
+++ project/app.php
@@ -28,7 +28,8 @@ if ($_SERVER['REDIRECT_URL'] == '/notification' && file_get_contents('php://inpu
 }
 
 if ($_SERVER['REDIRECT_URL'] == '/get-notification') {
-    echo (NotificationsModel::all())->toJson();
+    NotificationsModel::where('user_id', '=', '1')->first()->delete();
+//    echo (NotificationsModel::all())->toJson();
     die();
 }
 
