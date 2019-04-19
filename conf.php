31d99d3e923a163c6a8a83601978f3f6b835d9aa
mrgeorgegreen <mrgeorgegreen@hotmail.com>
2019-04-19 13:32:25 +0200

    Permission

--- conf.php
+++ conf.php
@@ -5,7 +5,7 @@ return [
         'client_id' => '8h5hefxsn94rxcvj37pib6aepnucla',
         'redirect_uri' => 'https://mrgreen-twitch-api.herokuapp.com/twitch',
         'response_type' => 'token',
-        'scope' => 'channel:read:subscriptions chat:read',
+        'scope' => 'channel:read:subscriptions chat:read user:read:email',
         'token_type' => 'bearer'
     ],
     'webhooks' => [
